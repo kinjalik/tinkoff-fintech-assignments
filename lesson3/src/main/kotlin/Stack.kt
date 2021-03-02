91a98c638d0f379426b2da018b344eef7206bc7b
Albert Akmukhametov <albert@kinjalik.ru>
2021-03-02 16:05:23 +0300

    Added KDod description

--- lesson3/src/main/kotlin/Stack.kt
+++ lesson3/src/main/kotlin/Stack.kt
@@ -1,12 +1,24 @@
+/**
+ * A LinkedList-based implementation of Stack ADT.
+ * @param T the type of stored values
+ */
 class Stack<T> : Collection<T> {
     override var size: Int = 0
     private var head: ListItem<T>? = null
 
+    /**
+     * Places the value on top of stack
+     * @param item the value to store
+     */
     fun push(item: T) {
         head = ListItem(item).also { it.setNext(head) }
         size++
     }
 
+    /**
+     * Removes the value from top of stack
+     * @return the value, that were just removed
+     */
     fun pop(): T? {
         val t = head?.getValue()
         head = head?.getNext()
@@ -14,21 +26,11 @@ class Stack<T> : Collection<T> {
         return t
     }
 
-    fun demoPrint() {
-        if (size == 0) {
-            println("Trying to demoPrint empty stack")
-            return
-        }
-        println("Current state of stack:")
-        var i = 0
-        var cur = head
-        while (cur != null) {
-            println("Item $i: ${cur.getValue()}")
-            cur = cur.getNext()
-        }
-        println("End of current state of stack")
-    }
-
+    /**
+     * Returns `true` if the value stored in stack
+     * @param element the value for check
+     * @return boolean indicator of containment
+     */
     override fun contains(element: T): Boolean {
         var cur = head
         while (cur != null && cur.getValue() != element)
@@ -37,15 +39,28 @@ class Stack<T> : Collection<T> {
         return cur != null
     }
 
+    /**
+     * Returns `true` if all items of `elements` stored in stack
+     * @param elements collection of items for checking
+     * @return boolean indicator
+     */
     override fun containsAll(elements: Collection<T>): Boolean {
         for (el in elements)
-            if (contains(el))
-                return true
-        return false
+            if (!contains(el))
+                return false
+        return true
     }
 
+    /**
+     * Returns `true` if stack is empty
+     * @return boolean indicator of emptiness
+     */
     override fun isEmpty(): Boolean = size == 0
 
+    /**
+     * Return an iterator over the stack
+     * @return iterator object
+     */
     override fun iterator(): Iterator<T> = object : Iterator<T> {
         var next = head
         override fun hasNext(): Boolean {
