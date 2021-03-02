2138d3b0d6a6d2ab211e494f6cfe64eddbb98b79
Albert Akmukhametov <albert@kinjalik.ru>
2021-03-02 15:53:11 +0300

    Added DLS methods

--- lesson3/src/main/kotlin/Main.kt
+++ lesson3/src/main/kotlin/Main.kt
@@ -1,46 +1,59 @@
 fun main() {
     val numberOfTestItems = 3
     println("Queue demonstration")
-    val q = Queue<String>()
+    var queueExample = Queue<String>()
 
-    println("Current size: ${q.size}")
+    println("Current size: ${queueExample.size}")
     for (i in 1..numberOfTestItems)
-        q.enqueue("Item #$i")
-    println("Current size: ${q.size}")
+        queueExample.enqueue("Item #$i")
+    println("Current size: ${queueExample.size}")
 
     println("Demo of iterator:")
-    for (el in q)
+    for (el in queueExample)
         println(el)
 
     println("Demo of contains:")
-    println("\"Item #1\" in queue: ${q.contains("Item #1")}")
-    println("\"Item #1337\" in queue: ${q.contains("Item #1337")}")
+    println("\"Item #1\" in queue: ${queueExample.contains("Item #1")}")
+    println("\"Item #1337\" in queue: ${queueExample.contains("Item #1337")}")
 
 
     println("Demo of dequeue:")
     for (i in 0..numberOfTestItems)
-        println(q.dequeue())
-    println("Current size: ${q.size}")
+        println(queueExample.dequeue())
+    println("Current size: ${queueExample.size}")
 
     println("----------------------------------")
     println("Stack demonstration")
-    val s = Stack<String>()
+    var stackExample = Stack<String>()
 
-    println("Current size: ${s.size}")
+    println("Current size: ${stackExample.size}")
     for (i in 1..numberOfTestItems)
-        s.push("Item #$i")
-    println("Current size: ${s.size}")
+        stackExample.push("Item #$i")
+    println("Current size: ${stackExample.size}")
 
     println("Demo of iterator:")
-    for (el in s)
+    for (el in stackExample)
         println(el)
 
     println("Demo of contains:")
-    println("\"Item #1\" in stack: ${s.contains("Item #1")}")
-    println("\"Item #1337\" in stack: ${s.contains("Item #1337")}")
+    println("\"Item #1\" in stack: ${stackExample.contains("Item #1")}")
+    println("\"Item #1337\" in stack: ${stackExample.contains("Item #1337")}")
 
     println("Demo of pop:")
     for (i in 0..numberOfTestItems)
-        println(s.pop())
-    println("Current size: ${s.size}")
+        println(stackExample.pop())
+    println("Current size: ${stackExample.size}")
+
+    println("----------------------------------")
+    println("DLS on queue demo")
+    queueExample = queueOf("first", "second", "third")
+    for (e in queueExample)
+        println(e)
+
+    println("----------------------------------")
+    println("DLS on Stack demo")
+    stackExample = stackOf("first", "second", "third")
+    for (e in stackExample)
+        println(e)
 }
+
