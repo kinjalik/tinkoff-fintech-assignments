8af723d71902fb1f92fd6557743d234ec0bf8aba
Albert Akmukhametov <albert@kinjalik.ru>
2021-03-02 15:05:52 +0300

    Revert "Lesson2"

--- lesson3/src/main/kotlin/ListItem.kt
+++
@@ -1,9 +0,0 @@
-class ListItem<T>(private val item: T) {
-    private var nextItem: ListItem<T>? = null
-
-    fun getValue() = item
-    fun getNext() = nextItem
-    fun setNext(next: ListItem<T>?) {
-        nextItem = next
-    }
-}
