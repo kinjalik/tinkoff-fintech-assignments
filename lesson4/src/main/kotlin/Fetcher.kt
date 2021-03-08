7cc7d8727b7cfad321718f6cda0c71999da3e7c9
Albert Akmukhametov <albert@kinjalik.ru>
2021-03-08 18:31:34 +0300

    Fixes according to feedback

--- lesson4/src/main/kotlin/Fetcher.kt
+++ lesson4/src/main/kotlin/Fetcher.kt
@@ -1,16 +1,19 @@
 class Fetcher(productDAO: ProductDAO, private val categoryDAO: CategoryDAO) {
 
     private val data: List<Product> = productDAO.selectAll().map {
-        Product(it.name, it.description, it.price, it.categoryIds.map { id -> categoryDAO.selectById(id) })
+        Product(it.id, it.name, it.description, it.price, it.categoryIds.mapNotNull { id -> categoryDAO.selectById(id) })
     }
 
     fun getData() = data
+    fun getProductById(id: Int): Product? = data.find {it.id == id}
 
     fun getSortedByPrice() = data.sortedBy { it.price }
 
-    fun getByCategory(category: Category): List<Product> = data.filter { category in it.categories }
-
-    fun getByCategoryId(id: Int): List<Product> = getByCategory(categoryDAO.selectById(id))
-
     fun countByPredicate(lambda: (Product) -> Boolean) = data.filter(lambda).size
+
+    fun getGroupedByCategory(): Map<Category, List<Product>> {
+        return data.flatMap { prod ->
+            prod.categories.map { cat -> Pair(prod.id, cat) }
+        }.groupBy { it.second }.entries.associate { entry -> entry.key to entry.value.mapNotNull { getProductById(it.first) } }
+    }
 }
--- lesson4/src/main/kotlin/Main.kt
+++ lesson4/src/main/kotlin/Main.kt
@@ -7,8 +7,11 @@ fun main() {
     println("#### FETCHING SORTED (by price) ####")
     fetcher.getSortedByPrice().onEach(::println)
 
-    println("#### FETCHING GROUPED (by category id 2) ####")
-    fetcher.getByCategoryId(2).onEach(::println)
+    println("#### FETCHING GROUPED ####")
+    fetcher.getGroupedByCategory().forEach { k, v ->
+        println("Key $k:")
+        println(v)
+    }
 
     println("#### FETCHING ELEMENT COUNT ####")
     fetcher.countByPredicate { it.price < 200 }.also(::println)
